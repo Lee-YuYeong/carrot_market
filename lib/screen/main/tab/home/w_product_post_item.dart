4429f148f3f09c2b26d98a75ad36453e02d291fb
Lee-YuYeong <dbdud312066@gmail.com>
2024-09-10 17:58:27 +0900

    style: cachednetworkimage적용

--- lib/screen/main/tab/home/w_product_post_item.dart
+++ lib/screen/main/tab/home/w_product_post_item.dart
@@ -1,6 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:fast_app_base/entity/post/vo_product_post.dart';
 import 'package:flutter/material.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
 class ProductPostItem extends StatelessWidget {
   final ProductPost post;
@@ -9,19 +9,22 @@ class ProductPostItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        CachedNetworkImage(
-          imageUrl: post.product.images[0],
-        ),
-        Expanded(
-          child: Column(
-            children: [
-              Text(post.content)
-            ],
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: CachedNetworkImage(imageUrl: post.product.images[0], width: 150)),
+          Expanded(
+            child: Column(
+              children: [
+                Text(post.content)
+              ],
+            )
           )
-        )
-      ],
+        ],
+      ),
     );
   }
 }
