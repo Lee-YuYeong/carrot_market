1bf402cb6c755f7ee831e032433312371b01e7c3
Lee-YuYeong <dbdud312066@gmail.com>
2024-09-18 14:44:16 +0900

    design: 메인 앱바 알림 아이콘 추가

--- lib/screen/main/tab/home/f_home.dart
+++ lib/screen/main/tab/home/f_home.dart
@@ -2,6 +2,7 @@ import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/entity/dummies.dart';
 import 'package:fast_app_base/screen/main/fab/w_floating_daangn_button.riverpod.dart';
 import 'package:fast_app_base/screen/main/tab/home/w_product_post_item.dart';
+import 'package:fast_app_base/screen/notification/s_notification.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -44,6 +45,14 @@ class _HomeFragmentState extends ConsumerState<HomeFragment> {
             itemBuilder: (context) => ["다트동", "앱동"].map((e) => PopupMenuItem(value: e,child: Text(e))).toList(),
             child: Text(title),
           ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Nav.push(const NotificationScreen());
+              }, 
+              icon: const Icon(Icons.notifications_none_rounded),
+            )
+          ],
         ),
         Expanded(
           child: ListView.separated(
